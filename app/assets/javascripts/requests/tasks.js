b62c088f2a5981459a4a73fe1c678be6bfe42c59
Justin <justinjackson1198@gmail.com>
2020-06-17 12:00:20 -0700

    fully integrated front end with back end api

--- app/assets/javascripts/requests/tasks.js
+++ app/assets/javascripts/requests/tasks.js
@@ -1,131 +1,113 @@
+var tasks = [];
+var state = 0;
+
 $.ajaxSetup({
   headers: {
     'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
   }
 });
 
-var indexTasks = function (successCB, errorCB) {
+//get Tasks list
+var indexTasks = function () {
   var request = {
     type: 'GET',
-    url: 'api/tasks?api_key=1',
-    success: successCB,
-    error: errorCB
+    url: '/api/tasks?api_key=1',
+    success: function (response, textStatus) {
+      tasks = response.tasks;
+      if (tasks) {
+      //checks if the task array is empty
+        $('.task').remove();
+        var tasksLeft = 0;
+        tasks.forEach(function (task) {
+          if (task.completed === false) {
+            tasksLeft++;
+          }
+          //default behavior
+          if (state === 0) {
+            renderTask(task);
+            if (task.completed === true) {
+              toggleCheck();
+            }
+          }
+          //active button pressed
+          else if (state === 1 && task.completed === true) {
+            renderTask(task);
+            toggleCheck();
+          }
+          //completed button pressed
+          else if (state === 2 && task.completed === false) {
+            renderTask(task);
+          }
+        });
+        $('.left-to-complete').text(String(tasksLeft) + ' items left');
+      }
+    },
+    error: ajaxError
   }
 
   $.ajax(request);
 };
 
 
-var postTask = function (content, successCB, errorCB) {
-  var request = {
-    type: 'POST',
-    url: 'api/tasks?api_key=1',
-    data: {
-      task: {
-        content: content
-      }
-    },
-    success: successCB,
-    error: errorCB
-  }
-
-  $.ajax(request);
-}
-
-//POST Request
 var postTask = function (event) {
   if (event.key === 'Enter') {
     var task = $(this).val();
-    console.log('Task: ' + task);
-    $.ajax({
+    var request = {
       type: 'POST',
-      url: 'api/tasks?api_key=1',
-      contentType: 'application/json',
-      dataType: 'json',
-      data: JSON.stringify({
+      url: '/api/tasks?api_key=1',
+      data: {
         task: {
           content: task
         }
-      }),
-      success: function (response, textStatus) {
-        console.log(response);
-        //update list
-        fireGET();
       },
-      error: function (request, textStatus, errorMessage) {
-        console.log(errorMessage);
-      }
-    });
+      success: indexTasks,
+      error: ajaxError
+    }
+
+    $.ajax(request);
     $(this).val('');
   }
 }
 
 //PUT Request
 var markComplete = function (id) {
-    $.ajax({
+    var request = {
       type: 'PUT',
-      url: 'api/tasks' + id + '/mark_complete?api_key=1',
+      url: '/api/tasks/' + id + '/mark_complete?api_key=1',
       dataType: 'json',
-      success: function (response, textStatus) {
-        fireGET();
-      },
-      error: function (req, textStatus, err) {
-        console.log(err);
-      }
-    });
+      success: indexTasks,
+      error: ajaxError
+    };
+
+    $.ajax(request);
 }
 
-var markActive = function (id, successCB, ajaxError) {
-    $.ajax({
+var markActive = function (id) {
+    var request = {
       type: 'PUT',
-      url: 'api/tasks' + id + '/mark_active?api_key=1',
+      url: '/api/tasks/' + id + '/mark_active?api_key=1',
       dataType: 'json',
-      success: indexTasks(renderTasks, ajaxError),
-      error: ajaxError()
-    });
+      success: indexTasks,
+      error: ajaxError
+    };
+
+    $.ajax(request);
 }
 
+var deleteTask = function (id) {
+    var request = {
+      type: 'DELETE',
+      url: '/api/tasks/' + id + '?api_key=1',
+      dataType: 'json',
+      success: indexTasks,
+      error: ajaxError
+    };
 
-  //GET Request
-var indexTasks = function (successCB, errorCB) {
-  $.ajax({
-    type: 'GET',
-    url: 'api/tasks?api_key=1',
-    dataType: 'json',
-    success: successCB,
-    error: errorCB
-  });
+    $.ajax(request);
 }
 
-var renderTasks = function (response, textStatus) {
-  tasks = response.tasks;
-  if (tasks) {
-  //checks if the task array is empty
-    $('.task').remove();
-    var tasksLeft = 0;
-    tasks.forEach(function (task) {
-      if (task.completed === false) {
-        tasksLeft++;
-      }
-      //default behavior
-      if (state === 0) {
-        taskUpdate(task);
-        if (task.completed === true) {
-          toggleCheck();
-        }
-      }
-      //active button pressed
-      else if (state === 1 && task.completed === true) {
-        taskUpdate(task);
-        toggleCheck();
-      }
-      //completed button pressed
-      else if (state === 2 && task.completed === false) {
-        taskUpdate(task);
-      }
-    });
-    $('.left-to-complete').text(String(tasksLeft) + ' items left');
-  }
+var renderTask = function (task) {
+  return $('<div class="row task pt-2">' + '<span class="ml-2 mt-2 check-box" data-id="' + task.id + '">' + '<i class="fas fa-check check-mark">' + '</i>' + '</span>' + '<h2 class="col-10">' + task.content + '</h2>' + '<span class="ml-auto mt-2 mr-2 cancel" data-id="' + task.id + '">' + '<p>' + 'x' + '</p>' + '</span>' + '</div>').insertBefore('.end');
 }
 
 var ajaxError = function (req, textStatus, err) {
--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,12 +1,47 @@
+var toggleCheck = function () {
+  $('.container').children().last().prev().children('.check-box').children().toggle('fast');
+};
+
+var toggleCancel = function () {
+  $(this).children('.cancel').toggle();
+}
+
 $(document).on("turbolinks:load", function () {
   if ($('.static_pages.index').length > 0) {
-    indexTasks(function (response) {
-      var htmlString = response.tasks.map(function (task) {
-        return "<div class='col-12 mb-3 p-2 border rounded task' data-id='" + task.id + "'> \
-          " + task.content + "\
-          </div>";
-      });
-      $("#Tasks").html(htmlString);
+    indexTasks();
+
+    $('input').keypress(postTask);
+
+    $(document).on('click', '.cancel', function () {
+      var id = $(this).data('id');
+      deleteTask(id);
     });
-  }
+
+  $(document).on('click', '.check-box', function () {
+    if ($(this).children().first().css('display') === 'none') {
+      markComplete($(this).data('id'));
+    }
+    else {
+      markActive($(this).data('id'));
+    }
+  });
+
+
+  $('.btn-all').click(function () {
+    state = 0;
+    indexTasks();
+  });
+
+  $('.btn-active').click(function () {
+    state = 2;
+    indexTasks();
+  });
+
+  $('.btn-completed').click(function () {
+    state = 1;
+    indexTasks();
+  });
+
+  $(document).on('mouseenter mouseleave', '.task', toggleCancel);
+  };
 });
