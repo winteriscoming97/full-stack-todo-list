9e02aea376f51785d5dbc05068d06dc1182552e5
Justin <justinjackson1198@gmail.com>
2020-06-17 08:03:23 -0700

    .

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,7 +1,7 @@
 $(document).on("turbolinks:load", function () {
   if ($('.static_pages.index').length > 0) {
-    indexTasks( response => {
-      var htmlString = response.tasks.map(task => {
+    indexTasks(function (response) {
+      var htmlString = response.tasks.map(function (task) {
         return "<div class='col-12 mb-3 p-2 border rounded task' data-id='" + task.id + "'> \
           " + task.content + "\
           </div>";
